eb1d8e4ef735c65802a3d06a143ba64094b093d2
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-25 11:26:41 -0300

    Update Relogio.cpp

--- relogio/src/Relogio.cpp
+++ relogio/src/Relogio.cpp
@@ -2,14 +2,35 @@
 #include <iostream>
 using namespace std;
 
-Relogio::Relogio(int horas, int minutos, int segundos)
-{
-	this->horas = horas;
-	this->minutos = minutos;
-	this->segundos = segundos;
+Relogio::Relogio(int horas, int minutos, int segundos){
+	this->setHoras(horas);
+	this->setMinutos(minutos);
+	this->setSegundos(segundos);
 }
 
-void Relogio::exibirHoras()
-{
+void Relogio::setHoras(unsigned int horas){
+	this->horas = horas;
+}
+
+unsigned int Relogio::getHoras(){
+	return this->horas;
+}
+	
+void Relogio::setMinutos(unsigned int minutos){
+	this->minutos = minutos;
+}
+unsigned int Relogio::getMinutos(){
+	return this->minutos;
+}
+	
+void Relogio::setSegundos(unsigned int segundos){
+	this->segundos = segundos;
+}
+unsigned int Relogio::getSegundos(){
+	return this->segundos;
+}
+
+void Relogio::exibirHoras(){
+	
 	cout << "Horario: " << this->horas << ":" << this->minutos << ":" << this->segundos << endl;
 }

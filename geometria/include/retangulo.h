c77d36748559963910c7eaac52806d3c07439929
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-25 11:31:04 -0300

    Update retangulo.h

--- geometria/include/retangulo.h
+++ geometria/include/retangulo.h
@@ -2,10 +2,16 @@
 class Retangulo{
 public:
 	Retangulo(int largura, int comprimento);
-	int largura;
-	int comprimento;
-
+	
+	void setLargura(largura);
+	unsigned int getLargura();
+	
+	void setComprimento(comprimento);
+	unsigned int getComprimento();
+	
 	void calculaEImprimeArea();
-
+private:
+	unsgined int largura;
+	unsgined int comprimento;
 };
 

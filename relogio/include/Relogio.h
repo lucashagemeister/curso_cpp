29e99ec3496e6af459f443953d55814ef9155e5d
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-25 11:20:28 -0300

    Update Relogio.h

--- relogio/include/Relogio.h
+++ relogio/include/Relogio.h
@@ -4,10 +4,22 @@ class Relogio
 {
 public:
 	Relogio(int horas, int minutos, int segundos);
-	int horas;
-	int minutos;
-	int segundos;
 	
+	void setHoras(unsigned int horas);
+	unsigned int getHoras();
+	
+	void setMinutos(unsigned int minutos);
+	unsigned int getMinutos();
+	
+	void setSegundos(unsigned int segundos);
+	unsigned int getSegundos();
+
 	void exibirHoras();
+private:
+	unsigned int horas;
+	unsigned int minutos;
+	unsigned int segundos;
+	
+	
 };
 

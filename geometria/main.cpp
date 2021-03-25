38f3aec8d9b85c803baffa1f16fd043b34f11098
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-25 10:55:38 -0300

    Create main.cpp

---
+++ geometria/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include "retangulo.h"
+using namespace std;
+
+int main() {
+	int l;
+	int c;
+	cout << "Diga o valor inteiro para largura do retangulo: ";
+	cin >> l;
+	cout << "Diga o valor inteiro para  comprimento do retangulo: ";
+	cin >> c;
+	Retangulo *r;
+	r = new Retangulo(l, c);
+	cout << "Area do retangulo: ";
+	cout << r->calculaArea() << endl;
+	system("PAUSE");
+	return 0;
+}

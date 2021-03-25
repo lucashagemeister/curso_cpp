73fdaa13c615d67ce0e345dfd1ac278e5ed60487
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-25 11:34:22 -0300

    Update retangulo.cpp

--- geometria/src/retangulo.cpp
+++ geometria/src/retangulo.cpp
@@ -4,9 +4,26 @@
 using namespace std;
 
 Retangulo::Retangulo(int largura, int comprimento) {
+    this->setLargura(largura);
+    this->setComprimento(comprimento);
+}
+
+void Retangulo::setLargura(largura){
     this->largura = largura;
+}
+
+unsigned int Retangulo::getLargura(){
+    return this->largura;
+}
+
+void Retangulo::setComprimento(comprimento){
     this->comprimento = comprimento;
 }
+
+unsigned int Retangulo::getComprimento(){
+    return this->comprimento;
+}
+
 void Retangulo::calculaEImprimeArea() {
     cout << "Area do retangulo: " << (this->largura) * (this->comprimento) << endl;
 }
